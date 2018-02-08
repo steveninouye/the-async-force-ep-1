4e8f0a61e221f7538b77ab09c12f2ee16a203c59
Steven Inouye <steveninouye@msn.com>
2018-02-07 21:32:54 -1000

    refacetored

--- js/app.js
+++ js/app.js
@@ -1,88 +1,54 @@
 console.log('Im running here');
 
-const Req = new XMLHttpRequest();
-
-function reqListener() {
-    const Req2 = new XMLHttpRequest();
-    const reqObj = JSON.parse(this.responseText);
-
-    function reqListener2() {
-        document.getElementById('person4HomeWorld').innerText = JSON.parse(this.responseText).name;
-    }
-
-    document.getElementById('person4Name').innerText = reqObj.name;
-
-    Req2.addEventListener('load', reqListener2);
-    Req2.open('GET', reqObj.homeworld);
-    Req2.send();
+function request(url, callback) {
+  const Req = new XMLHttpRequest();
+  Req.addEventListener('load', callback);
+  Req.open('GET', url);
+  Req.send();
+  //   return reqObj;
 }
 
-Req.addEventListener("load", reqListener);
-Req.open("GET", "https://swapi.co/api/people/4/");
-Req.send();
+request('https://swapi.co/api/people/4/', function() {
+  const reqObj = JSON.parse(this.responseText);
+  document.getElementById('person4Name').innerText = reqObj.name;
+  request(reqObj.homeworld, function() {
+    document.getElementById('person4HomeWorld').innerText = JSON.parse(
+      this.responseText
+    ).name;
+  });
+});
 
-const Req1 = new XMLHttpRequest();
+request('https://swapi.co/api/people/14', function() {
+  const reqObj = JSON.parse(this.responseText);
+  document.getElementById('person14Name').innerText = reqObj.name;
+  request(reqObj.species, function() {
+    document.getElementById('person14Species').innerText = JSON.parse(
+      this.responseText
+    ).name;
+  });
+});
 
-function reqListener1() {
-    const Req2 = new XMLHttpRequest();
-    const reqObj = JSON.parse(this.responseText);
-
-    function reqListener2() {
-        document.getElementById('person14Species').innerText = JSON.parse(this.responseText).name;
-    }
-
-    document.getElementById('person14Name').innerText = reqObj.name;
-
-    Req2.addEventListener('load', reqListener2);
-    Req2.open('GET', reqObj.species);
-    Req2.send();
-}
-
-Req1.addEventListener('load', reqListener1);
-Req1.open('GET', 'https://swapi.co/api/people/14');
-Req1.send();
-
-const Req3 = new XMLHttpRequest();
-
-function reqListener3() {
-    const reqObj = JSON.parse(this.responseText);
-    // console.log(reqObj.results);
-    reqObj.results.forEach(e => {
-        document.getElementById('filmList').innerHTML +=
-            `<li class = 'film'>
+request('https://swapi.co/api/films', function() {
+  const reqObj = JSON.parse(this.responseText);
+  reqObj.results.forEach(e => {
+    document.getElementById('filmList').innerHTML += `<li class = 'film'>
                 <h2 class = 'filmTitle' > ${e.title}</h2>
                 <h3>Planets</h3>
                     <ul class = 'filmPlanets' id = '${e.title}'>
-                        
-                    </ul>               
+
+                    </ul>
             </li>
             `;
-        let title = e.title;
-        e.planets.forEach(f => {
-            const Req2 = new XMLHttpRequest();
-            const reqObj = JSON.parse(this.responseText);
-
-            function reqListener2() {
-                let reqObj2 = JSON.parse(this.responseText);
-                document.getElementById(e.title).innerHTML +=
-                    `
+    let title = e.title;
+    e.planets.forEach(f => {
+      request(f, function() {
+        let reqObj2 = JSON.parse(this.responseText);
+        document.getElementById(e.title).innerHTML += `
                 <li class='planet'>
                 <h4 class = 'planetName'>${reqObj2.name}</h4>
                 </li>
-                `
-            }
-
-            Req2.addEventListener('load', reqListener2);
-            Req2.open('GET', f);
-            Req2.send();
-        }
-        )
-    })
-
-
-
-}
-
-Req3.addEventListener('load', reqListener3);
-Req3.open('GET', 'https://swapi.co/api/films/');
-Req3.send();
+                `;
+      });
+    });
+  });
+});
